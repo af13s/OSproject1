6232d92dbb7f7c2bba91a3fb0e0f742a89d85e70
af13s <af13s@my.fsu.edu>
2018-02-04 21:29:30 -0500

    solid test run

--- execute.c
+++ execute.c
@@ -15,7 +15,7 @@ void execute(struct PCMD cmd)
         }
       else if (pid == 0)
         {
-            printf("child was created");
+            //printf("child was created");
           //Execute command in new process
         	//int redir_type, int pipe_num, normal, built_in, background
         	if (cmd.redir_type != 0)
@@ -30,8 +30,8 @@ void execute(struct PCMD cmd)
         		return;
         	}
 
-            printf("reached execution stage");
-            //execv(cmd.CMD1[0],cmd.CMD1);
+            //printf("reached execution stage");
+            execv(cmd.CMD1[0],cmd.CMD1);
         }
       else
         {

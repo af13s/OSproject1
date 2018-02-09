4b920667230afed81965e363e7ab201d103eb431
Eduardo Alvarez <eea13c@my.fsu.edu>
2018-02-09 17:41:59 -0500

    (More Changes to readme, initial stuff for io built-in)

--- exec_builtIn.c
+++ exec_builtIn.c
@@ -1,4 +1,5 @@
 #include "functions.h"
+
 //built_in #define EXIT 0 // #define CD 1 // #define ECHO 2 // #define ETIM 3 // #define IO 4
 
 void exec_builtin(struct PCMD cmds)
@@ -18,8 +19,21 @@ void exec_builtin(struct PCMD cmds)
          break;
 
       case IO :
-         //printf("Better try again\n" );
-         break;
+	pid_t pid;
+        if ((pid = fork()) < 0)
+            printf("there was an error");
+        else if (pid == 0)
+        {
+            	cmds.CMD1[0], cmds.CMD1; // child executes command
+		exit(1); // exits
+        }
+        else
+        {	
+		char* argv[] = {"/bin/cat", "/proc/pid/io"};	
+		execv(argv[0] , argv); 
+                call_wait(pid,cmds);
+        }
+         
 
       default :
          printf("Invalid built_in\n" );
